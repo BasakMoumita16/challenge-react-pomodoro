e48ee85b9364cda62accf1a809a766158a68e1ba
BasakMoumita16 <moumitaduttabasak@gmail.com>
2020-01-30 11:05:55 +0100

    changes made

---
+++ challenge_pomodoro/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Stopwatch from "./Stopwatch";
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <div className="App-title"></div>
+          <div className="Timers">
+            <Stopwatch />
+        
+        </div>
+    </div>
+  );
+}
+
+export default App;
